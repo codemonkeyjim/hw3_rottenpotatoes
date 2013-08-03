26c090be1a0cdf955b038bb4c5ac24279415655a
Jim Pfleger <jpfleger@acm.org>
2013-08-03 10:09:02 -0700

    Implement "some ratings" scenario

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -15,12 +15,8 @@ Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   flunk "Unimplemented"
 end
 
-# Make it easier to express checking or unchecking several boxes at once
-#  "When I uncheck the following ratings: PG, G, R"
-#  "When I check the following ratings: G"
-
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(/\s+/).each do |rating|
+    step %{I #{uncheck}check "ratings_#{rating}"}
+  end
 end
